6a354ce9b27e6a0d8b1b66b5342ebb5b385fab9c
Irina Fadeeva <if7129@gmail.com>
2020-03-12 18:13:20 +0300

    add brain-even

---
+++ src/games/even.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace BrainGames\games\even;
+
+use function BrainGames\Cli\run;
+
+function isEven($number)
+{
+    return $number % 2 === 0;
+}
+
+function gameEven($gameRepeatCount = 3)
+{
+    $rule = "Answer \"yes\" if the number is even, otherwise answer \"no\".";
+    $data = [];
+
+    for ($i = 0; $i < $gameRepeatCount; $i++) {
+        $question = rand(1, 100);
+        $correctAnswer = isEven($question) ? 'yes' : 'no';
+        $data[] = [$question, $correctAnswer];
+    }
+    run($rule, $data);
+}
