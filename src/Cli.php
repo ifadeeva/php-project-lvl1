e930c1840340ac6fc082ebad4ea21b38f9b40592
Irina Fadeeva <if7129@gmail.com>
2020-03-12 17:26:53 +0300

    add brain-even

--- src/Cli.php
+++ src/Cli.php
@@ -1,13 +1,30 @@
 <?php
 
-namespace BrainGames\cli;
+namespace BrainGames\Cli;
 
 use function cli\line;
 use function cli\prompt;
 
-function run()
+function run($rules, $data)
 {
     line("Welcome to the Brain Games!");
+    line($rules);
     $name = prompt("May I have your name?");
     line("Hello, %s!", $name);
+    line();
+
+    foreach ($data as $roundData) {
+        [$question, $correctAnswer] = $roundData;
+        line("Question: ", $question);
+        $answer = prompt("Your answer: ");
+        if ($answer === $correctAnswer) {
+            line("Correct!");
+        } else {
+            line("'", $answer, "' is wrong answer ;(. Correct answer was '", $correctAnswer, "'.");
+            line("Let's try again, ", $name, "!");
+            return false;
+        }
+    }
+    line("Congratulations, ", $name, "!");
+    return true;
 }
